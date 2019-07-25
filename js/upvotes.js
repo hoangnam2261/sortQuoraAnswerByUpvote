f93ac4ed2e2084731a2103f302f75e8cdfb00a50
hoangnam2261 <51202261@stu.hcmut.edu.vn>
2019-07-25 13:12:40 +0700

    Update index of classes for a quora's change.

--- js/upvotes.js
+++ js/upvotes.js
@@ -36,7 +36,7 @@ function getAnswerCount(answer) {
 }
 
 
-var answerListOutter = document.getElementsByClassName('UnifiedAnswerPagedList PagedListFoo unified')[1];
+var answerListOutter = document.getElementsByClassName('UnifiedAnswerPagedList PagedListFoo unified')[0];
 var answerList = answerListOutter.getElementsByClassName('paged_list_wrapper')[0];
 
 //removed collapsed answers
